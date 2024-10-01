e326b8279b1f5799bb8794b1256a25890a61fcd9
pmihango <pmihango@gmail.com>
2024-10-01 17:37:21 +0300

    updated to give it a grid look

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const display = document.getElementById('display');
 
 // Append number to the display when a number button is clicked
 function appendNumber(number) {
-    // If the display is showing 0, replace it, otherwise append the number
     if (display.value === '0') {
         display.value = number;
     } else {
@@ -29,10 +28,8 @@ function deleteChar() {
 // Calculate the result of the expression shown on the display
 function calculate() {
     try {
-        // Use eval to calculate the mathematical expression
         display.value = eval(display.value);
     } catch (error) {
-        // If there is an error (e.g., invalid input), display an error message
         display.value = 'Error';
     }
 }
